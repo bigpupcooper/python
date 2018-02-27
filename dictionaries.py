6e7f1dbdda191a3428bb57f2aeaacb2701001a0d
Tim McCartney <timmc41@hotmail.com>
2018-02-27 14:41:54 +0000

    Looping dictionary Key or Value

--- dictionaries.py
+++ dictionaries.py
@@ -99,7 +99,7 @@ print("The following languages, have been mentioned:")
 for language in favorite_languages.values():
     print(language.title())
 
-# The above will loop all and show duplicates, so use set()
+# The above will loop all and show duplicates, so use set() as seen below->
 
 favorite_languages = {
     'jen': 'python',
@@ -110,6 +110,59 @@ favorite_languages = {
 print("The following languages, have been mentioned:")
 for language in set(favorite_languages.values()):
     print(language.title())
-    #
+
+
+#Nesting-
+#storing a set of dictionaries in a LIST
+#or a list of items inside a dictionary, even dic in dic
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+
+######
+aliens = []
+#Make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color':'green','points': 5, 'speed':'slow'}
+    aliens.append(new_alien)
+
+#show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+#Show how many aliens have been created
+print("Total number of aliens: " + str(len(aliens)))
+
+print(aliens)
+
+##Now slice through and change on the first three
+aliens = []
+#Make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color':'green','points': 5, 'speed':'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+#show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+print("...")
 
 

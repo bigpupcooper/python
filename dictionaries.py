aff585198a4415984cd0cb24bdfc0ed998f6bf66
Tim McCartney <timmc41@hotmail.com>
2018-02-27 12:17:36 +0000

    Looping dictionary Key or Value

--- dictionaries.py
+++ dictionaries.py
@@ -46,3 +46,69 @@ print ("Sarah's favorite language is " +
        favorite_languages['sarah'].title() +
        ".")
 
+###########Looping in dictionary#
+#method items() returns a list of key-value pairs
+
+user_0 = {
+    'username':'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+    }
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+#applying the looping thru all key-value pairs
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+    }
+for name, language in favorite_languages.items():
+print(name.title() + "'s favorite language is " +
+    language.title() + ".")
+
+#Looping through all the KEYS in a dict
+#use the keys() method
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+    }
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        print("Hi" + name.title() +
+            ", I see your fav language is " +
+            favorite_languages[name].title())
+
+#Looping thru all VALUES in a dict
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+    }
+print("The following languages, have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+# The above will loop all and show duplicates, so use set()
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+    }
+print("The following languages, have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+

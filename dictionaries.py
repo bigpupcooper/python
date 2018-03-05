2f3ac3a76b61cd101e0a5397150f9e7afa550d82
Tim McCartney <timmc41@hotmail.com>
2018-03-05 13:36:47 +0000

    list in a dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -166,3 +166,17 @@ for alien in aliens[:5]:
 print("...")
 #
 
+#### A list in a Dictionary
+
+    # Example- store information about a pizza being ordered.
+    pizza = {
+        'crust': 'thick',
+        'toppings': ['mushrooms', 'extra cheese']
+
+    }
+    # Summarize the order.
+    print("You ordered a " + pizza['crust'] + "-crust pizza  +"
+                                              " with the following toppings:")
+    for topping in pizza['toppings']:
+        print("\t" + topping)
+

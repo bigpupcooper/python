c37e593840bb390b6d8973f8504e1b0020ddbc3e
Tim McCartney <timmc41@hotmail.com>
2018-03-08 10:51:29 +0000

    user input and while loops, with prompts and flags

---
+++ user_input_and_while_loops.py
@@ -0,0 +1,42 @@
+message = raw_input("Tell me something to repeat back:")
+print(message)
+
+#use the += to build a prompt over muliple lines
+prompt = "If you tell us who you are, we can personalize the messages you see"
+prompt += "\nWhat is you first name? "
+name = raw_input(prompt)
+print(name)
+
+####input treated as a string, so convert it by using int()
+age = raw_input("How old are you")
+age = int(age)
+
+
+#################
+##While loops###
+
+    current_number = 1
+    while current_number <= 5:
+        print(current_number)
+        current_number = current_number + 1
+
+###keep running until user asks to quit
+prompt = "\nTell me something"
+prompt += "\nEnter 'quit' to end"
+
+message = ""
+while message != 'quit':
+    message = raw_input(prompt)
+    print(message)
+
+####Using a FLAG
+prompt = "\nTell me something"
+prompt += "\nEnter 'quit' to end"
+
+active = True
+while active:
+    message = raw_input(prompt)
+    if message == 'quit' :
+        active = False
+    else:
+        print(message)
